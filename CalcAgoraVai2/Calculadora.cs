fc705a00080c90b4cab2a67d41e532aacdc9b5cb
naoniluz <naoni.luz@flex.com>
2021-05-07 14:55:43 -0300

    first commit

---
+++ CalcAgoraVai2/Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using CalcAgoraVai2.Exceptions;
+
+namespace CalculadoraNaoni {
+    class Calculadora {
+        public static double Calcular(string expressao) {
+
+            string[] valores = expressao.Split(' ');
+
+            double n1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            char sinal = char.Parse(valores[1]);
+            double n2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            double resultado = 0;
+
+            switch (sinal) {
+                case '+':
+                    resultado = Soma(n1, n2);
+                    break;
+                case '-':
+                    resultado = Sub(n1, n2);
+                    break;
+                case '*':
+                    resultado = Mult(n1, n2);
+                    break;
+                case '/':
+                    resultado = Divi(n1, n2);
+                    break;
+            }
+
+            return resultado;
+
+
+        }
+
+
+        public static double Soma(double a, double b) {
+            return a + b;
+        }
+
+        public static double Sub(double a, double b) {
+            return a - b;
+        }
+
+        public static double Mult(double a, double b) {
+            return a * b;
+        }
+
+        public static double Divi(double a, double b) {
+            if (b == 0) {
+                Console.WriteLine("Não é possível fazer divisão por zero, tente outra vez");
+            }
+            return a / b;
+        }
+
+    }
+}
---
+++ CalcAgoraVai2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CalculadoraNaoni {
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("-- Bem Vindo a Calculadora Padawan Naoni! -- ");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Insira seus dados no seguinte formato: ");
+            Console.WriteLine("    número     operador     número     ");
+            Console.WriteLine();
+            Console.WriteLine("Se quiser parar a execução digite SAIR");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+
+            Boolean sair = false;
+            //var expressao = string.Empty;
+
+
+            while (!sair) {
+                try {
+                    string expressao = Console.ReadLine();
+
+
+
+
+
+                    if (expressao == "SAIR") {
+                        sair = true;
+                    }
+                    else {
+
+                        double resultado = Calculadora.Calcular(expressao);
+                        Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture));
+                    }
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Entrada invalida, por favor tente novamente");
+                }
+            }
+        }
+    }
+    }
